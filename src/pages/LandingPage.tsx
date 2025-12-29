a0672406a87628468e65dfe6de4a3b2c55eb0995
Þórarinn Hjálmarsson <thorarinnhjalmarsson@gmail.com>
2025-12-29 00:16:31 +0000

    Copy: Change '14 daga endurgreiðslu' to '14 daga prufa'

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -108,7 +108,7 @@ export default function LandingPage() {
                             </div>
                             <div className="flex items-center gap-2">
                                 <CheckCircle className="w-5 h-5 text-green-400" />
-                                <span>14 daga endurgreiðslu</span>
+                                <span>14 daga prufa</span>
                             </div>
                         </div>
                     </div>
