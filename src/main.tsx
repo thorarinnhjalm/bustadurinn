b98509abe2ad87ebc12d135e5200c26ab07afdc5
Þórarinn Hjálmarsson <thorarinnhjalmarsson@gmail.com>
2025-12-29 16:41:41 +0000

    chore: force deploy trigger

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Force deploy check
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
